151291f11dd10bd42748f212e39f9ae15de3f066
MinhHoan147 <hoan.bm14720@gmail.com>
2023-01-06 01:02:25 +0700

    update

---
+++ Project3_Elearning/LMS_PROJECT/LMS/LMS/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import redirect, render
+
+def BASE(request):
+    return render(request, 'base.html')
+
+def HOME(request):
+    return render(request, 'Main/home.html')
+
+def SINGLE_COURSE(request):
+    return render(request, 'Main/single_course.html')
+
+def CONTACT_US(request):
+    return render(request, 'Main/contact_us.html')
+
+def ABOUT_US(request):
+    return render(request, 'Main/about_us.html')
+
+def LOGIN(request):
+    return render(request, 'Registration/login.html')
+
+def REGISTER(request):
+    return render(request, 'Registration/register.html')
